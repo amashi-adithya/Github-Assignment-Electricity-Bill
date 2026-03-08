1b29d10d4767bb83c05869405b4e882aa3e0eb7f
Amashi <amashigg03@gmail.com>
2026-03-08 15:39:44 +0530

    Added Output display for the bill

--- electricity.py
+++ electricity.py
@@ -2,3 +2,8 @@ print("Electricity Bill Calculator")
 client_type = input("Enter consumer type (domestic/corporate/religious): ")
 units = float(input("Enter electricity units used: "))
 bill = 0
+
+# Output
+print("Consumer Type:", client_type)
+print("Electricity Units:", units)
+print("Total Bill: Rs.", bill)
