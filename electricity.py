c175048f380e293fd6f75fd79d182f3d1212b00d
Amashi <amashigg03@gmail.com>
2026-03-08 15:33:10 +0530

    Added the Bill Calculation Logic

--- electricity.py
+++ electricity.py
@@ -2,3 +2,22 @@ print("Electricity Bill Calculator")
 client_type = input("Enter consumer type (domestic/corporate/religious): ")
 units = float(input("Enter electricity units used: "))
 bill = 0
+
+#Calculation Feature
+if client_type == 'domestic':
+    if units <= 100:
+        bill = units * 10
+    elif units <= 200:
+        bill = (100 * 10) + (units - 100) * 15
+    else:
+        bill = (100 * 10) + (100 * 15) + (units - 200) * 20
+
+elif consumer_type == "corporate":
+    bill = units * 25
+
+elif consumer_type == "religious":
+    bill = units * 5
+
+# SSCL Tax
+sscl = bill * 0.025
+total_bill = bill + sscl
